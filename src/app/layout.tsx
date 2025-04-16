ecc6c939924f900e91a6ffd92f067a82688479d7
priyansh-narang2308 <145008344+priyansh-narang2308@users.noreply.github.com>
2025-04-16 12:20:37 +0530

    database connected and done

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import ConvexClerkProvider from "@/providers/ConvexWithClerkProvider";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "CodeFlex AI",
+  description: "AI Fitness Trainer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ConvexClerkProvider>
+
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ConvexClerkProvider>
+  );
+}
